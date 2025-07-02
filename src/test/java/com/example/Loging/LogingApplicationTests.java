5d3dfbab2a0c42d6d70370bc3b05648b753ecfea
lochanajayasinghe <159622233+lochanajayasinghe@users.noreply.github.com>
2025-07-02 19:55:25 +0530

    create folder

---
+++ src/test/java/com/example/Loging/LogingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Loging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
