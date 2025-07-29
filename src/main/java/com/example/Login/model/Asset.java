057e56046b79c3d4cc89bbce884c0dc028a1fabe
Lochana Jayasinghe <lochanajayasinghe2002@gmail.com>
2025-07-29 13:58:13 +0530

    update asset view section

--- src/main/java/com/example/Login/model/Asset.java
+++ src/main/java/com/example/Login/model/Asset.java
@@ -36,28 +36,40 @@ public class Asset {
     // Getters and Setters
     public String getAssetId() { return assetId; }
     public void setAssetId(String assetId) { this.assetId = assetId; }
+
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+
     public String getType() { return type; }
     public void setType(String type) { this.type = type; }
+
     public String getSerialNumber() { return serialNumber; }
     public void setSerialNumber(String serialNumber) { this.serialNumber = serialNumber; }
+
     public String getModel() { return model; }
     public void setModel(String model) { this.model = model; }
+
     public String getSpecification() { return specification; }
     public void setSpecification(String specification) { this.specification = specification; }
+
     public java.time.LocalDate getPurchaseDate() { return purchaseDate; }
     public void setPurchaseDate(java.time.LocalDate purchaseDate) { this.purchaseDate = purchaseDate; }
+
     public boolean isActivityStatus() { return activityStatus; }
     public void setActivityStatus(boolean activityStatus) { this.activityStatus = activityStatus; }
+
     public String getWarrantyId() { return warrantyId; }
     public void setWarrantyId(String warrantyId) { this.warrantyId = warrantyId; }
+
     public java.time.LocalDate getWarrantyDate() { return warrantyDate; }
     public void setWarrantyDate(java.time.LocalDate warrantyDate) { this.warrantyDate = warrantyDate; }
+
     public String getWarrantyPeriod() { return warrantyPeriod; }
     public void setWarrantyPeriod(String warrantyPeriod) { this.warrantyPeriod = warrantyPeriod; }
+
     public String getPurchaseStore() { return purchaseStore; }
     public void setPurchaseStore(String purchaseStore) { this.purchaseStore = purchaseStore; }
+
     public Location getLocation() { return location; }
     public void setLocation(Location location) { this.location = location; }
 }
