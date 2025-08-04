d401027610e1a938c182252f991a687942e4366d
Lochana Jayasinghe <lochanajayasinghe2002@gmail.com>
2025-08-04 20:00:56 +0530

    Update H_AssetController.java

--- src/main/java/com/example/Login/controller/H_AssetController.java
+++ src/main/java/com/example/Login/controller/H_AssetController.java
@@ -2,14 +2,12 @@ package com.example.Login.controller;
 
 import com.example.Login.model.Asset;
 import com.example.Login.service.H_AssetService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class H_AssetController {
