e9ff2012da2991eb94e7fe859206dc0b147ee03a
Lochana Jayasinghe <lochanajayasinghe2002@gmail.com>
2025-08-06 21:15:32 +0530

    Create WebMvcConfig.java

---
+++ src/main/java/com/example/Login/config/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package com.example.Login.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/profile-photos/**")
+                .addResourceLocations("file:uploads/profile-photos/");
+    }
+}
