fc0a46526fe2080037e63ca823ee395b237a3644
Lochana Jayasinghe <lochanajayasinghe2002@gmail.com>
2025-08-10 03:21:57 +0530

    Update S_CondemnRepository.java

--- src/main/java/com/example/Login/repository/S_CondemnRepository.java
+++ src/main/java/com/example/Login/repository/S_CondemnRepository.java
@@ -4,4 +4,5 @@ import com.example.Login.model.Condemn;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface S_CondemnRepository extends JpaRepository<Condemn, String> {
+    Condemn findTopByAsset_AssetIdOrderByCondemnDateDesc(String assetId);
 }
