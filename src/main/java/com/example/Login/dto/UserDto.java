c43193cb73bfa7d2ce32f4330b5a80f1472142ff
Lochana Jayasinghe <lochanajayasinghe2002@gmail.com>
2025-08-10 23:44:32 +0530

    Update UserDto.java

--- src/main/java/com/example/Login/dto/UserDto.java
+++ src/main/java/com/example/Login/dto/UserDto.java
@@ -10,14 +10,16 @@ public class UserDto {
     private String username;
     private String fullName;
     private String email;
+    private String profilePhotoUrl;
     private Set<String> roles;
     private boolean enabled;
 
-    public UserDto(Long id, String username, String fullName, String email, Set<String> roles, boolean enabled) {
+    public UserDto(Long id, String username, String fullName, String email, String profilePhotoUrl, Set<String> roles, boolean enabled) {
         this.id = id;
         this.username = username;
         this.fullName = fullName;
         this.email = email;
+        this.profilePhotoUrl = profilePhotoUrl;
         this.roles = roles;
         this.enabled = enabled;
     }
